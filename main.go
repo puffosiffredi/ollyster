af16151871f6069a708a35929f67691f42703746
LowEel <loweel@gmx.de>
2015-11-05 01:27:06 +0100

    Another little step

--- main.go
+++ main.go
@@ -13,7 +13,8 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/static/", ServeStatic)
-	mux.HandleFunc("/", hello)
+	mux.HandleFunc("/network/", ServeNetwork)
+	mux.HandleFunc("/", home)
 
 	log.Fatal(http.ListenAndServe(":8181", mux))
 }
@@ -31,7 +32,8 @@ func Hpwd() string {
 
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
+//cath-all function for sending people back to homepage.
+func home(w http.ResponseWriter, r *http.Request) {
 
 	http.Redirect(w, r, "/static", http.StatusMovedPermanently)
 
@@ -39,11 +41,22 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// to manage static contents
 func ServeStatic(w http.ResponseWriter, r *http.Request) {
 
 	HttpRoot := Hpwd()
 	log.Println("DocumentRoot: ", HttpRoot)
 	log.Println("Serving: ", r.URL.Path)
 	http.ServeFile(w, r, HttpRoot+r.URL.Path)
+
+}
+
+//to hide logics behind of network.
+func ServeNetwork(w http.ResponseWriter, r *http.Request) {
+
+	HttpRoot := Hpwd()
+	log.Println("DocumentRoot: ", HttpRoot)
+	log.Println("Serving: ", r.URL.Path)
+	http.ServeFile(w, r, HttpRoot+"/static/network.html")
 
 }
